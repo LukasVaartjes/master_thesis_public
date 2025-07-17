fd37f74e241cf54e657c5ca64f3106b63f311c47
LukasVaartjes <68281102+LukasVaartjes@users.noreply.github.com>
2025-07-17 15:05:27 +0200

    modify bat file for running creation of dataset

--- create_dataset.bat
+++ create_dataset.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Running calculate_global_z_min_max.py...
-python code/calculate_global_z_min_max.py
+python code/create_dataset/calculate_global_z_min_max.py
 if %errorlevel% neq 0 (
     echo Script calculate_global_z_min_max.py failed. Exiting...
     pause
@@ -8,12 +8,20 @@ if %errorlevel% neq 0 (
 )
 
 echo Running generate_segments_pointcloud_greyscale.py...
-python code/generate_segments_pointcloud_greyscale.py
+python code/create_dataset/generate_segments_pointcloud_greyscale.py
 if %errorlevel% neq 0 (
     echo Script generate_segments_pointcloud_greyscale.py failed.
     pause
     exit /b %errorlevel%
 )
 
-echo All scripts completed successfully.
+echo Running split_traing_validate_test_dataset.py...
+python code/create_dataset/split_traing_validate_test_dataset.py
+if %errorlevel% neq 0 (
+    echo Script split_traing_validate_test_dataset.py failed.
+    pause
+    exit /b %errorlevel%
+)
+
+echo All scripts completed successfully, dataset created
 pause
